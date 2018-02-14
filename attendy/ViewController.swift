bc462101c79275d12a4c029701e590e0b1a45390
Rohan Konnur <rohankonnur@berkeley.edu>
2018-02-14 14:28:06 -0800

    Initial Commit

---
+++ attendy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  attendy
+//
+//  Created by Rohan Konnur on 2/14/18.
+//  Copyright © 2018 Rohan Konnur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
